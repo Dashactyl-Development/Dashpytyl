d82dc7617091bd02c0ca88ad000337f6c904ba96
Shersha <87840878+Shersha01@users.noreply.github.com>
2021-09-11 18:32:54 +0530

    Now get custom port

--- api/configfile.py
+++ api/configfile.py
@@ -13,6 +13,7 @@ with open('settings.json', 'r') as cfg:
     settings = json.load(cfg)
     discordsettings = settings["discord"]
     pterosettings = settings["pterodactyl"]
+    websettings = settings["website"]
 
 clientID = discordsettings['application_id']
 clientSecret = discordsettings['secret_key']
@@ -20,6 +21,8 @@ redirectURI = discordsettings['redirect_uri']
 
 pteroURL = pterosettings['url']
 pteroAppKey= pterosettings['key']
+
+webport = websettings['port']
 
 with open('api\\users.json') as usrs:
     users = json.load(usrs)
