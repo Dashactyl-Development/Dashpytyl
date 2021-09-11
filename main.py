aeedb4bdf77bc4dc4592fd62364d9b27eb52c90b
Shersha <87840878+Shersha01@users.noreply.github.com>
2021-09-11 18:34:21 +0530

    Added support for custom port.

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ def createuser():
     return render_template("dashboard.html", user=user, panellink=configfile.pteroURL)
 
 if __name__ == "__main__":
-	app.run(debug=True,port=5000)
+	app.run(debug=True,port=configfile.webport)
