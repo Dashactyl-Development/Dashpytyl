dad0448ab83ac09f156fb68857959499b7cd8bc4
Shersha <87840878+Shersha01@users.noreply.github.com>
2021-09-12 12:59:49 +0530

    Update userdata.py

--- api/userdata.py
+++ api/userdata.py
@@ -6,7 +6,7 @@ import requests
 from urllib.parse import quote
 from pydactyl import PterodactylClient
 from requests.exceptions import HTTPError
-
+import sqlite3
 """
 Creates Pterodactyl users and checks
 if they exist. If they don't, it makes an account
@@ -14,6 +14,8 @@ Make a pull request if you think anything can be improved.
 """
 
 
+
+  
 with open('settings.json', 'r') as cfg:
     settings = json.load(cfg)
     discordsettings = settings["discord"]
@@ -50,10 +52,33 @@ def create_user(discordUsername, discordEmail, discordUserID, discorddic):
         "Authorization": f"Bearer {pteroAppKey}",
         "Accept": "application/json",
         "Content-Type": "application/json",
-        "cookie": "pterodactyl_session=eyJpdiI6InhIVXp5ZE43WlMxUU1NQ1pyNWRFa1E9PSIsInZhbHVlIjoiQTNpcE9JV3FlcmZ6Ym9vS0dBTmxXMGtST2xyTFJvVEM5NWVWbVFJSnV6S1dwcTVGWHBhZzdjMHpkN0RNdDVkQiIsIm1hYyI6IjAxYTI5NDY1OWMzNDJlZWU2OTc3ZDYxYzIyMzlhZTFiYWY1ZjgwMjAwZjY3MDU4ZDYwMzhjOTRmYjMzNDliN2YifQ%253D%253D"
     }
 
     payload = '{"email": "'+str(discordEmail)+'","username": "'+str(discordUserID)+'","first_name": "'+str(discordUsername)+'","last_name": "'+str(discorddic)+'"}'
 
-    response = requests.request('POST', url, data=payload, headers=headers)
-    print(response.text)
+    requests.request('POST', url, data=payload, headers=headers)
+    headers2 = {
+    "Authorization": f"Bearer {pteroAppKey}",
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    }
+    data2=requests.get(f"{pteroURL}/api/application/users?filter[email]{str(f'={discordEmail}')}", headers=headers2)
+    response = data2.json()
+    if data2.status_code == 200:
+        db = sqlite3.connect('database.db')
+        cursor = db.cursor()
+        cursor.execute(f"SELECT id FROM main WHERE username = {discordUserID}")
+        result = cursor.fetchone()
+        for i in response["data"]:
+            id = i["attributes"]["id"]
+        if result == None:
+            sql = (f"INSERT INTO main(id, username) VALUES (?, ?)")
+            val = (id, discordUserID)
+            cursor.execute(sql,val)
+        else:
+            pass
+        db.commit()
+        cursor.close()
+        db.close()
+    else:
+        pass
